7ddbd8ee4799b7f8963e14db64280725814ccae9
Jyotiraditya Khatua <jyotiraditya20122004@gmail.com>
2026-02-20 09:23:21 +0530

    Add attendance data for students

---
+++ attendence.py
@@ -0,0 +1,13 @@
+{
+    "name": "Deepak Rawat",
+    "enrollment": "23478838282",
+    "courses": "DA",
+    "phone": "9818774323"
+  },
+  {
+    "name": "Rajesh",
+    "enrollment": "73273283273823",
+    "courses": "DA",
+    "phone": "983947394739493"
+  }
+]
